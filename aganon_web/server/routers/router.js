09e24824ef2d26614213a6f7f3c68c410406ebda
exponent112 <21800181@handong.edu>
2020-06-12 20:09:43 +0900

    nation

--- aganon_web/server/routers/router.js
+++ aganon_web/server/routers/router.js
@@ -12,13 +12,19 @@ router.get('/api/getUsername', (req, res, next) => {
 router.post('/ict', (req, res) => {
     var sql = "SELECT ?? ";
     console.log(req.body);
+    var nation = [];
+    for(var index_n = 1; index_n<=21; index_n++){
+        var s_nation = 'checked'+index_n;
+        req.body.s_nation ?  nation.push(req.body.s_nation.parseInt): '';
+    }
+
     var qry = [];
     req.body.nation_id ?  qry.push('nation_id'): '';
     req.body.year ?  qry.push('year') :  '';
     req.body.internet_users ?  qry.push('internet_users') :  '';
     req.body.mobile_subscribers ?  qry.push('mobile_subscribers') :  '';
     req.body.main_telephone_lines ?  qry.push('main_telephone_lines') :  '';
-    
+    if(nation.length == 0) res.send();
     if(qry.length == 0){
         res.send();
     }else{
@@ -26,6 +32,12 @@ router.post('/ict', (req, res) => {
             sql = sql + ", ?? "
     }
     sql = sql + "FROM `ICT`";
+    sql = sql + "WHERE";
+    for(var j = 0 ; j<nation.length-1; j++){
+        sql = sql + "nation_id =" + nation[j] +" OR ";
+    }
+    sql = sql + "nation_id =" + nation[nation.length-1];
+
     
     sql = mysql.format(sql, qry);
     console.log(sql);
